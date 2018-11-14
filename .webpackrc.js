29a42ed0c100263dd8e1208f48aab51bf0ab2633
殷浩楠 <yinhaonan@kg.com>
2018-11-14 15:51:29 +0800

    yhn 11.14

--- .webpackrc.js
+++ .webpackrc.js
@@ -4,10 +4,14 @@
 //     //return path.join(__dirname, '..', dir)
 //   }
 
+console.log(123123);
+console.log(process.env)
 export default {
-    disableCSSModules:true
+  define: {
+    "process.env": {
+        NODE_ENV: process.env.NODE_ENV,
+        yhn: process.env.yhn
+    },
+  },
+  disableCSSModules: true,
 }
-
-
-
-
--- src/routes/aaa.js
+++ src/routes/aaa.js
@@ -1,66 +1,131 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'dva';
 import classnames from 'classnames'
 import '../assets/sass/yhn.scss'
-
+import PropTypes from 'prop-types';
 
 
 class Clock extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        name: "11111",
-        a:1,
-        b:2,
-        time:new Date(),
-        redFlag:true,
-      };
-    }
 
-    componentDidMount(){
-      console.log(this.props);
-    }
-   dis=()=>{
-      this.props.dispatch({
-        type: 'aaa/save',
-        payload: {
-          aaa: 111,
-        }
-      });
-    }
+  static childContextTypes = {
+    wenzi: PropTypes.string
+  }
 
-   eff=()=>{
-      this.props.dispatch({
-        type: 'aaa/effects1',
-        payload: {
-          aaa: 'fromEffects1',
-          effects1:50,
-        }
-      });
-    }
-
-    showProps(){
-      console.log(Object.values(this.props.aaa));
-
-      let values=Object.values(this.props.aaa).map(item=><div key={item}>{item}</div>)
-      return (<div>{values}</div>)
-    }
-
-    red(){
-      return 'red'
-    }
-
-    render() {
-      return (
-        <div className="App">
-          <h1 className={classnames({'red':this.state.redFlag},'fs30')}>首页</h1>
-          <h2>{this.showProps()}</h2>
-          <button onClick={this.dis}>dispatch</button>
-          <button onClick={this.eff}>effects</button>
-        </div>
-      );
+  getChildContext = () => {
+    return {
+      wenzi: this.state.b
     }
   }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "11111",
+      a: 1,
+      b:'ContextFromClock',
+      time: new Date(),
+      redFlag: true,
+    };
+  }
+
+  componentDidMount() {
+    console.log(123);
+    //console.log(process)
+    console.log(Promise);
+    console.log(process.env)
+    //console.log(this.props);
+  }
+  dis = () => {
+    this.props.dispatch({
+      type: 'aaa/save',
+      payload: {
+        aaa: 111,
+      }
+    });
+  }
+
+  eff = () => {
+    this.props.dispatch({
+      type: 'aaa/effects1',
+      payload: {
+        aaa: 'fromEffects1',
+        effects1: 50,
+      }
+    });
+  }
+
+  showProps() {
+    console.log(Object.values(this.props.aaa));
+
+    let values = Object.values(this.props.aaa).map(item => <div key={item}>{item}</div>)
+    return (<div>{values}</div>)
+  }
+
+  red() {
+    return 'red'
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className={classnames({ 'red': this.state.redFlag }, 'fs30')}>首页(父组件)</h1>
+        <h2>{this.showProps()}</h2>
+        <button onClick={this.dis}>dispatch</button>
+        <button onClick={this.eff}>effects</button>
+        <Son></Son>
+      </div>
+    );
+  }
+}
+
+
+
+class Son extends Component {
+
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div className="son">
+        <div>子组件</div>
+        <Sun></Sun>
+      </div>
+    );
+  }
+}
+
+class Sun extends Component {
+  static contextTypes = {
+    wenzi: PropTypes.string
+  }
+
+  constructor(props){
+    super(props)
+    this.state={
+      num:10
+    }
+  }
+
+  componentDidMount() {
+    console.log(this)
+  }
+
+
+  render() {
+
+    return (
+      <div className="sun">
+        <div>孙组件</div>
+        <div>
+          <input  style={{width:"50px"}} type="text" onChange={(e)=>{}} value={this.state.num}/>
+          <p>{this.context.wenzi}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 
 
@@ -68,9 +133,8 @@ Clock.propTypes = {
 };
 
 
-function mapStateToProps({aaa}) {
-  return {aaa};
+function mapStateToProps({ aaa }) {
+  return { aaa };
 }
-
 
 export default connect(mapStateToProps)(Clock);
